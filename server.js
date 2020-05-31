0f0a0f1e7f41c487f61f350cdec5714a7662a1b9
xiaolu sun <xiaolusun@xiaolus-MBP.fios-router.home>
2020-05-31 10:32:28 -0400

    add MONGODB_URI

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
-
+  useNewUrlParser: true,
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+
   useFindAndModify: false
 });
 
